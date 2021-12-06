f38c3aff2d94ea013d69089f14d96fda7d746c45
celmore25@gmail.com <celmore25@gmail.com>
2021-12-06 13:09:49 -0700

    actions

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
 
+requirements = ['pandas>=1.0.0']
+
 setup(
     name="timewarpy",
     version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=requirements,
 )
