3ffea4bcd7324886ae74e03de8bb264137ac39a6
celmore25@gmail.com <celmore25@gmail.com>
2021-12-06 09:58:02 -0700

    setting up repo

--- timewarpy/tests/test_preprocess.py
+++ timewarpy/tests/test_preprocess.py
@@ -1,7 +1,9 @@
 from .. import preprocess
 
+
 def test_increment():
     assert preprocess.increment(3) == 4
 
+
 def test_decrement():
-    assert preprocess.decrement(3) == 4
+    assert preprocess.decrement(3) == 2
