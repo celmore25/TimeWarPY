3e3541d51d1b629803f1c79078e5834d5f8a5f5b
celmore25@gmail.com <celmore25@gmail.com>
2021-12-07 16:48:27 -0700

    dataset docs

--- timewarpy/datasets.py
+++ timewarpy/datasets.py
@@ -6,6 +6,8 @@ def load_energy_data():
     """Loads a multi-variate time series dataset from the timewarpy library
     for energy usage forecasting.
 
+    More information can be found [here](https://archive.ics.uci.edu/ml/datasets/Appliances+energy+prediction)
+
     Returns:
         pandas.DataFrame: time series dataset
     """
