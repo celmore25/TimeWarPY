40c483e21009acfa0a8788857232eaae4c603c75
celmore25 <celmore25@users.noreply.github.com>
2021-12-08 23:07:07 +0000

    Bump version: 0.0.15 → 0.0.16

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
 
 setup(
     name='timewarpy',
-    version='0.0.15',
+    version='0.0.16',
     description='Time series processing framework and utilities for deep learning',
     long_description=long_description,
     long_description_content_type='text/markdown',
